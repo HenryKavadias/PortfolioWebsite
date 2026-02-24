b51e97829fd5e06beed2817e9ab020cf47b0e3e5
Henry Kavadias-Barnes <bkyrneh@gmail.com>
2026-02-24 17:15:46 +1100

    Revert "Started adding features"
    This reverts commit ec4a85db7902a5e56b73972cd07341edd80979b7.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,11 @@
+import { useState, useEffect } from "react";
 
-import ParagraphContent from "../components/ParagraphContent";
-
-import { TextFileRenderer } from "../contexts/TextFileRenderer";
 
 function Home() {
     
-
-
     return (
         <div>
             <h1>Henry Kavadias-Barnes</h1>
-            <ParagraphContent content={
-                TextFileRenderer("/src/assets/AboutMe.txt")} />
         </div>
     );
 }
