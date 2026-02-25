884ad383648ba4e4e9901f71e7727042c35d9419
Henry Kavadias-Barnes <bkyrneh@gmail.com>
2026-02-25 11:26:59 +1100

    Add NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,26 @@ import DodgeWest from "./pages/DodgeWest";
 import FriendInMe from "./pages/FriendInMe";
 import EggEscape from "./pages/EggEscape";
 import GambitAndTheAnchored from "./pages/GambitAndTheAnchored";
+import NavBar from "./components/NavBar";
 import './css/App.css';
 
 function App() {
   
 
   return (
-    <main className='main-content'>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/purger" element={<Purger />} />
-        <Route path="/dodgewest" element={<DodgeWest />} />
-        <Route path="/friendinme" element={<FriendInMe />} />
-        <Route path="/eggescape" element={<EggEscape />} />
-        <Route path="/gambitandtheanchored" element={<GambitAndTheAnchored />} />
-      </Routes>
-    </main>
+    <>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/purger" element={<Purger />} />
+          <Route path="/dodgewest" element={<DodgeWest />} />
+          <Route path="/friendinme" element={<FriendInMe />} />
+          <Route path="/eggescape" element={<EggEscape />} />
+          <Route path="/gambitandtheanchored" element={<GambitAndTheAnchored />} />
+        </Routes>
+      </main>
+    </>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,17 @@
 import { Link } from 'react-router-dom';
+import '../css/NavBar.css';
 
 function NavBar() {
     return (
         <nav className='navbar'>
-            <ul>
-                <li><Link to="/" className="nav-link">Home</Link></li>
-                <li><Link to="/purger" className="nav-link">Projects</Link></li>
-                <li><Link to="/dodgeWest" className="nav-link">Dodge West</Link></li>
-                <li><Link to="/friendinme" className="nav-link">Friend In Me</Link></li>
-                <li><Link to="/eggescape" className="nav-link">Egg Escape</Link></li>
-                <li><Link to="/gambitandtheanchored" className="nav-link">Gambit And The Anchored</Link></li>
-            </ul>
+            <div className="navbar-links">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/purger" className="nav-link">Projects</Link>
+                <Link to="/dodgeWest" className="nav-link">Dodge West</Link>
+                <Link to="/friendinme" className="nav-link">Friend In Me</Link>
+                <Link to="/eggescape" className="nav-link">Egg Escape</Link>
+                <Link to="/gambitandtheanchored" className="nav-link">Gambit And The Anchored</Link>
+            </div>
         </nav>
     );
 }
