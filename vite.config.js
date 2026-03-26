32fb7f5cdd8ed9a46ffd5ebc92d49f7bde220ef8
Henry Kavadias-Barnes <bkyrneh@gmail.com>
2026-03-26 17:59:10 +1100

    removing an error for local testing

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const base = process.env.NODE_ENV === 'production' ? '/PortfolioWebsite/' : '/';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/PortfolioWebsite/'
+  base,
+  define: {
+    __APP_BASE__: JSON.stringify(base)
+  }
 })
